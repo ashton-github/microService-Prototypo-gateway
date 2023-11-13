8477d3a7908c07fa785bb63b97624eeb71757ba0
a.turki <at.aslem@gmail.com>
2023-11-13 10:16:40 +0100

    V0 init gateawy service

---
+++ src/main/java/com/proto/microServicePrototypogateway/MicroServicePrototypoGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.proto.microServicePrototypogateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicePrototypoGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServicePrototypoGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proto/microServicePrototypogateway/MicroServicePrototypoGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proto.microServicePrototypogateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServicePrototypoGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
